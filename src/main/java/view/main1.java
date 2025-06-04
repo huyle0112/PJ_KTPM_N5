0fd19ff36bee093b665b88443cdf6bb83d5ca421
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-06-04 23:41:15 +0700

    update charge view ui

--- src/main/java/view/main1.java
+++ src/main/java/view/main1.java
@@ -11,7 +11,7 @@ public class main1  extends Application {
         @Override
         public void start(Stage primaryStage) throws Exception {
             FXMLLoader fxmlLoader = new FXMLLoader(LoginMain.class.getResource("/ResidentChargeView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
             primaryStage.setScene(scene);
             primaryStage.show();
         }
