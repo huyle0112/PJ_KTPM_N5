5060ae0bdfac4b0bdceb6987a5b7f705cda3ab27
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-06-05 21:28:21 +0700

    improve UX

--- src/main/java/application/CLIMain.java
+++ src/main/java/application/CLIMain.java
@@ -5,10 +5,6 @@ import model.Citizen;
 
 public class CLIMain {
     public static void main(String[] args) {
-        CitizenController citizenController = new CitizenController();
-        Citizen c = new Citizen();
-        c.setFullname("Manh");
-        c.setResidencyStatus(Citizen.ResidencyStatus.Permanent);
-        c.
+
     }
 }
